800174e9f1754dac1c451894229064a495f5b09c
joshuakristian <55868330+joshuakristian@users.noreply.github.com>
2025-07-22 17:02:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,31 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:7050', 'https://untarx.com'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: ['http://localhost:7050', 'https://untarx.com', 'http://localhost:3005'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
+
+app.options('*', cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'assets')));
 
 const router = require('./app/Route.js');
 app.use('/api', router);
 
+
 app.use((req, res) => {
   res.status(404).json({ error: 'Not Found' });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
