8fb1939936120d40a20d79903bebb0cf2066b503
joshuakristian <55868330+joshuakristian@users.noreply.github.com>
2025-07-22 11:25:01 +0700

    Update db.js

--- app/db/db.js
+++ app/db/db.js
@@ -21,23 +21,35 @@ const mysql = require('mysql2');
 
 let config;
 try {
-    config = require('../../config');
+  config = require('../../config');
 } catch (e) {
-    console.log('No config file found');
-    process.exit(0);
+  console.log('No config file found');
+  process.exit(0);
 }
 
 function connect() {
-    const connection = mysql.createConnection(config.db);
-    return connection;
+  const connection = mysql.createConnection(config.db);
+  return connection;
 }
 
 function disconnect(connection) {
-    connection.end();
+  connection.end();
+}
+
+async function query(sql, params = []) {
+  const connection = connect();
+  try {
+    const [rows] = await connection.promise().query(sql, params);
+    return rows;
+  } finally {
+    disconnect(connection);
+  }
 }
 
 module.exports = {
-    connect,
-    disconnect,
-    escape: mysql.escape
-}
+  connect,
+  disconnect,
+  query,
+  escape: mysql.escape
+};
+
