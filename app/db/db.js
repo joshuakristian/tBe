b4ec472f50dc263b75ffc23edebd404c73337f9f
joshuakristian <55868330+joshuakristian@users.noreply.github.com>
2025-07-22 11:33:24 +0700

    Update db.js

--- app/db/db.js
+++ app/db/db.js
@@ -1,33 +1,21 @@
-// const mysql = require('mysql2')
+const mysql = require('mysql2')
 
 // const host = '31.97.220.30'
 // const username = 'untarx'
 // const password = '1m2E0i0sT02@{78386}!'
 // const db_name = 'te'
+const config = require('../../config')
 
-// const connection = mysql.createConnection({
-//   host: host,
-//   user: username,
-//   password: password,
-//   database: db_name,
+const connection = mysql.createConnection({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
 
-//   dateStrings: true,
-// })
+  dateStrings: true,
+})
 
-// connection.connect()
-
-// module.exports = connection
-const mysql = require('mysql2');
-
-let config;
-try {
-  config = require('../../config');
-} catch (e) {
-  console.log('No config file found');
-  process.exit(0);
-}
-
-const connection = mysql.createConnection(config.db);
+connection.connect()
 
 module.exports = connection
 
