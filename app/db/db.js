c5f2543f4a131a4295dc78c71307813cd2098d38
joshuakristian <55868330+joshuakristian@users.noreply.github.com>
2025-07-22 11:50:42 +0700

    db

--- app/db/db.js
+++ app/db/db.js
@@ -1,51 +1,20 @@
-// const mysql = require('mysql2')
+const mysql = require('mysql2')
 
-// const host = '31.97.220.30'
-// const username = 'untarx'
-// const password = '1m2E0i0sT02@{78386}!'
-// const db_name = 'te'
+const host = '31.97.220.30'
+const username = 'untarx'
+const password = '1m2E0i0sT02@{78386}!'
+const db_name = 'te'
 
-// const connection = mysql.createConnection({
-//   host: host,
-//   user: username,
-//   password: password,
-//   database: db_name,
+const connection = mysql.createConnection({
+  host: host,
+  user: username,
+  password: password,
+  database: db_name,
 
-//   dateStrings: true,
-// })
+  dateStrings: true,
+})
 
-// connection.connect()
+connection.connect()
 
-// module.exports = connection
-const mysql = require('mysql2');
-
-let config;
-try {
-    config = require('../../config');
-} catch (e) {
-    console.log('No config file found');
-    process.exit(0);
-}
-
-function connect() {
-    const connection = mysql.createConnection({
-        host: config.db.host,
-        user: config.db.user,
-        password: config.db.password,
-        database: config.db.database,
-        dateStrings: true,
-    });
-
-    return connection;
-}
-
-function disconnect(connection) {
-    connection.end();
-}
-
-module.exports = {
-    connect,
-    disconnect,
-    escape: mysql.escape 
-};
+module.exports = connection
 
