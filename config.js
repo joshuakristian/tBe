d689ddc4a773c2870fbc197093f47a2cbbca7bba
joshuakristian <55868330+joshuakristian@users.noreply.github.com>
2025-07-22 09:18:27 +0700

    Just SOme addjustment

---
+++ config.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const env = process.env
+
+const config = {
+  url: env.URL,
+  host: env.HOST,
+  port: env.PORT,
+  db: {
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    connectTimeout: env.DB_TIMEOUT,
+  },
+  mail: {
+    host: env.MAIL_HOST,
+    port: env.MAIL_PORT,
+    user: env.MAIL_USER,
+    password: env.MAIL_PASSWORD,
+  },
+}
+
+module.exports = config
--- index.js
+++ index.js
@@ -4,27 +4,41 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const app = express();
 
-// Konfigurasi CORS untuk development
 app.use(cors({
-  origin: true, // Allow all origins
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
-
-app.use(express.json())
-app.use(cors())
 const router = require('./app/Route.js')
-app.use(bodyParser.json() );   
+app.use(bodyParser.json());   
 app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.json())
+// app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'assets')))
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Methods',
+    'POST, GET, OPTIONS, PUT, DELETE, HEAD',
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'custId, appId, Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN, Access-Control-Allow-Origin, X-Requested-With',
+  )
+  res.header('Access-Control-Expose-Headers', 'Authorization, authenticated')
+  res.header('Access-Control-Max-Age', '1728000')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
+})  
+
 app.use('/api', router)
 
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
